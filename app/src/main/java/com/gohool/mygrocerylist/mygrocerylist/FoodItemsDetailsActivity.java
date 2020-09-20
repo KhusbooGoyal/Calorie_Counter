8dd186d12e2a59eb10154280d8b6beb0dfa698fe
Khusboo <khusboo1299@gmail.com>
2020-09-20 18:26:27 +0530

    App- Calorie Counter

--- app/src/main/java/com/gohool/mygrocerylist/mygrocerylist/FoodItemsDetailsActivity.java
+++ app/src/main/java/com/gohool/mygrocerylist/mygrocerylist/FoodItemsDetailsActivity.java
@@ -1,15 +1,20 @@
 package com.gohool.mygrocerylist.mygrocerylist;
 
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import data.DatabaseHandler;
 import model.Food;
 
 public class FoodItemsDetailsActivity extends AppCompatActivity {
@@ -68,5 +73,44 @@ public class FoodItemsDetailsActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Please install email client before sending",
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.deleteItem) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(FoodItemsDetailsActivity.this);
+            alert.setTitle("Delete?");
+            alert.setMessage("Are you sure you want to delete this item?");
+            alert.setNegativeButton("No", null);
+            alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    DatabaseHandler dba = new DatabaseHandler(getApplicationContext());
+                    dba.deleteFood(foodId);
+
+                    Toast.makeText(getApplicationContext(), "Food Item Deleted", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(FoodItemsDetailsActivity.this, DisplayFoodsActivity.class));
+
+                    //remove this activity from activity stack
+                    FoodItemsDetailsActivity.this.finish();
+                }
+            });
+            alert.show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
