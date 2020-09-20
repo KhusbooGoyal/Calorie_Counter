6a6af6a10933005ed6f029a6c1d76a697738cfb2
Khusboo <khusboo1299@gmail.com>
2020-09-20 12:16:38 +0530

    App- Calorie Counter

---
+++ app/src/main/java/com/gohool/mygrocerylist/mygrocerylist/FoodItemsDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.gohool.mygrocerylist.mygrocerylist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodItemsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_items_details);
+    }
+}
